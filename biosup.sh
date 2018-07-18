290f8452458a8749f5f39a0e9489408c797642dd
invulmain <39603833+invulmain@users.noreply.github.com>
2018-07-18 18:27:30 +0700

    1

--- biosup.sh
+++ biosup.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-atiflash -p 0 /home/user/RadeonRX580-4G-SK_Hynix_mod.rom -f
+atiflash -p 0 /home/user/biosup/RadeonRX580-4G-SK_Hynix_mod.rom -f
 
-atiflash -p 1 /home/user/RadeonRX570-4G-SK_Hynix_mod.rom -f
-atiflash -p 2 /home/user/RadeonRX570-4G-SK_Hynix_mod.rom -f
+atiflash -p 1 /home/user/biosup/RadeonRX570-4G-SK_Hynix_mod.rom -f
+atiflash -p 2 /home/user/biosup/RadeonRX570-4G-SK_Hynix_mod.rom -f
 
-atiflash -p 3 /home/user/RadeonRX580-4G-SK_Hynix_mod.rom -f
-atiflash -p 4 /home/user/RadeonRX580-4G-SK_Hynix_mod.rom -f
-atiflash -p 5 /home/user/RadeonRX580-4G-SK_Hynix_mod.rom -f
+atiflash -p 3 /home/user/biosup/RadeonRX580-4G-SK_Hynix_mod.rom -f
+atiflash -p 4 /home/user/biosup/RadeonRX580-4G-SK_Hynix_mod.rom -f
+atiflash -p 5 /home/user/biosup/RadeonRX580-4G-SK_Hynix_mod.rom -f
 
-atiflash -p 6 /home/user/RadeonRX570-4G-SK_Hynix_mod.rom -f
+atiflash -p 6 /home/user/biosup/RadeonRX570-4G-SK_Hynix_mod.rom -f
 
 exit 0
 
